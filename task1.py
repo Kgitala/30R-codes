a2cbbcff87c7f94ea02a17b65f60360c28bef889
Kagitala <pujithakagitala128@gmail.com>
2025-01-21 21:20:36 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,2 +1,32 @@
-123456,234567
-print(123456+234567)
+#Arthimetic operations
+print(5+7)
+print(5-7)
+print(5*7)
+print(5/7)
+print(5//7)
+
+#using variables
+x=123
+y=234
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+
+#strings
+a="pujitha"
+
+#length of str
+print(len(a))
+
+#type of str
+print(type(a))
+
+#indexing
+print(a[4])
+print(a[0])
+print(a[1:5])
+
+#
