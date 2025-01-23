f3e070b8ad7dd1c4d81bef0ca3bc1d9f4842f618
Kagitala <pujithakagitala128@gmail.com>
2025-01-23 08:20:03 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -29,4 +29,22 @@ print(a[4])
 print(a[0])
 print(a[1:5])
 
-#
+#slicing with +ve and -ve index
+a='josvika pujitha josvika'
+print(a[0:6])
+print(a[7:0:-2])
+print([-1:-22:-1])
+print(id(a))
+#represents same memory address
+print(id(j))
+print(id(p))
+#complex
+a1=1+2i
+a2=2+3i
+print(a1+a2)
+print(a1-a2)
+print(a1*a2)
+print(a1/a2)
+print(a1**a2)
+print(a1//a2)
+
