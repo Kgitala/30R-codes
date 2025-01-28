6475d03a9ca40ccca3bbce5494d1c77e8d5c23c4
Kagitala <pujithakagitala128@gmail.com>
2025-01-28 13:59:13 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,37 @@
+#Arthematic operations
+print(5+7)
+print(5-7)
+print(5*7)
+print(5/7)
+print(5//7)
+
+#using variables
+a=123
+b=234
+print(a + b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+
+#strings
+a="pujitha"
+
+
+#length of str
+print(len(a))
+
+#type of str
+print(type(a))
+
+#indexing
+print(a[4])
+print(a[0])
+print(a[1:5])
+
+#slicing with +ve and -ve index
+a='josvika pujitha josvika'
+print(a[0:6])
+print(a[7:0:-2])
+print(id(a))
