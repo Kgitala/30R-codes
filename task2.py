aad8785efed17d8e57e66697bca548916d6171c3
Kagitala <pujithakagitala128@gmail.com>
2025-02-06 13:30:53 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,68 @@
+#Arthimetic operations
+print(5+7)
+print(5-7)
+print(5*7)
+print(5/7)
+print(5//7)
+
+#using variables
+x=123
+y=234
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x**y)
+
+#strings
+a="pujitha"
+
+#length of str
+print(len(a))
+
+#type of str
+print(type(a))
+
+#indexing
+print(a[4])
+print(a[0])
+print(a[1:5])
+
+#slicing with +ve and -ve index
+a='josvika pujitha josvika'
+print(a[0:6])
+print(a[7:0:-2])
+print([-22,-1])
+print(id(a))
+#represents same memory address
+print(id(a))
+print(id(a))
+
+#print(a1//a2)
+atm_pin=123
+print(atm_pin ==122)
+print(atm_pin ==123)
+print(2.5>2)
+print(2.5<2)
+print(2.5>=2)
+user_password='1234'
+user_name='1233'
+if True:
+ print("login successfully")
+if False:
+ print("wrong password")
+#list
+list_1=[1,2,3,4,"string",True,[2,3,4]]
+print(type(list_1))
+print(list_1)
+print(list_1[2])
+print(len(list_1))
+print(list_1[0:3])
+#tuple
+tuple_1=(1,2,3,"string")
+print(type(tuple_1))
+print(len(tuple_1))
+print(tuple_1[3])
+
+
